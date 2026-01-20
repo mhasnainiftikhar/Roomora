f4171edf4271ce3b51ea450723049c091e87b527
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2026-01-20 14:51:46 +0500

    Setup the Backend

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
