ee4196d7337e59d294018effc14a0139a961d9f7
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2026-01-20 15:19:51 +0500

    Integration with Database

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import connectDB from './config/db.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
