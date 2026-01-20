f58d59fdcf7240d1c6585cff028eeea4e817b134
Muhammad Hasnain Iftikhar <160528829+mhasnainiftikhar@users.noreply.github.com>
2026-01-20 17:08:37 +0500

    Refactor

--- server/controllers/ClerkWebhook.js
+++ server/controllers/ClerkWebhook.js
@@ -4,51 +4,51 @@ import 'dotenv/config';
 import e from "express";
 
 export const handleClerkWebhook = async (req, res) => {
-   try {
-    // Initialize the Svix Webhook with your signing secret
-     const Whook = new Webhook(process.env.CLERK_WEBHOOK_SIGNING_SECRET);
-     //Getting Headers
+    try {
+        // Initialize the Svix Webhook with your signing secret
+        const Whook = new Webhook(process.env.CLERK_WEBHOOK_SIGNING_SECRET);
+        //Getting Headers
      const headers={
-    "svix-id": req.headers["svix-id"],
-    "svix-timestamp": req.headers["svix-timestamp"],
-    "svix-signature": req.headers["svix-signature"]
-     };
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        };
 
      //Verify the Headers
      await Whook.verify(JSON.stringify(req.body), headers);
 
-     //Getting data from the request body
-     const { type, data } = req.body;
+        //Getting data from the request body
+        const { type, data } = req.body;
 
      const userData={
-        _id: data.id,
-        username: data.first_name + " " + data.last_name,
-        email: data.email_addresses[0].email_address,
-        image: data.profile_image_url,
-     }
-     //Switch case for different event types
-     switch (type) {
+            _id: data.id,
+            username: data.first_name + " " + data.last_name,
+            email: data.email_addresses[0].email_address,
+            image: data.profile_image_url,
+        }
+        //Switch case for different event types
+        switch (type) {
         case "user.created":{
-            await User.create(userData);
-            break;
-        }
+                await User.create(userData);
+                break;
+            }
         case "user.updated":{
-            await User.findByIdAndUpdate(data.id, userData);
-            break;
-        }       
+                await User.findByIdAndUpdate(data.id, userData);
+                break;
+            }
         case "user.deleted":{
-            await User.findByIdAndDelete(data.id);
-            break;
+                await User.findByIdAndDelete(data.id);
+                break;
+            }
+            default:
+                break;
         }
-        default:
-            break;
-     }  
         res.json({success:true, message:"Webhook processed successfully"});
 
-   } catch (error) {
+    } catch (error) {
     console.error( error.message);
-    res.status(400).json({ success: false, message: "Invalid webhook event" });
-   }
+        res.status(400).json({ success: false, message: "Invalid webhook event" });
+    }
 };
 
 export default handleClerkWebhook;
